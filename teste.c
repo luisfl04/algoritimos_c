e672e99dc193c68a94c1936eab713e9cb81aa31a
@luisfl04_ <luisfelipecontato08@gmail.com>
2024-05-22 20:47:56 -0300

    feat: Estudos dia 22/05 -> Questão 2 -> Exercicio 10 -> Registros -> Algoritimos e programação estruturada

--- teste.c
+++ teste.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int obter_tamanho_do_vetor(int vetor[]){
+    
 
-    // Criando váriável que irá obter o tamanho;
-    int tamanho_do_vetor = 0;
-
-    // Laço 'for' que irá obter o tamanho 
-    for(int iteracao = 0; iteracao < vetor[]; iteracao++){
-        tamanho_do_vetor++;
-    }
-
-    return tamanho_do_vetor;
 }
 
 int main(){
 
     int vetor_de_exemplo[] = {1,2,3};
-
+    
     int resultado = obter_tamanho_do_vetor(vetor_de_exemplo);
 
     printf("%d", resultado); 
